cbac4329c6138e80a5b24707838490766a2258fb
zlayapsina <alex.tsaregorodcev1@gmail.com>
2023-10-30 03:44:48 +0300

    commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,24 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDao;
+import web.service.CarService;
 
 @Controller
 
 public class CarsController {
-    private final CarDao carDao;
+    private final CarService carService;
 
-    public CarsController(CarDao carDao) {
-        this.carDao = carDao;
+    public CarsController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-       if(count != null) {
-           model.addAttribute("cars", carDao.carCount(count));
-       } else {
-           model.addAttribute("cars", carDao.carList());
-       }
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,13 +1,15 @@
-package web.dao;
+package web.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 @Component
-public class CarDao {
+public class CarServiceImpl implements CarService {
     private static int CAR_COUNT;
     private List<Car> listCars;
 
@@ -20,12 +22,12 @@ public class CarDao {
         listCars.add(new Car(++CAR_COUNT, "lada", 98));
     }
 
-    public List<Car> carCount(int count) {
-        return listCars.subList(0, count);
-    }
-
-    public List<Car> carList() {
-        return listCars;
+    public List<Car> getCars(Integer count) {
+        if (count != null) {
+            return listCars.subList(0, count);
+        } else {
+            return listCars;
+        }
     }
 
 }
